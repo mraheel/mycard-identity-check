a26ac9ba3cc677624e76336bfa944848bac86840
muhammad.raheel <muhammad.raheel@pitb.gov.pk>
2025-06-25 23:40:54 +0500

    Update In Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,47 +1,72 @@
 import { useFonts } from "expo-font";
-// import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 import { Drawer } from "expo-router/drawer";
+import { ActivityIndicator, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+
+  const onlyShowVerificationScreen: boolean = false;
+  const [fontsLoaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
   }
 
+ 
   return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer>
-        <Drawer.Screen
-            name="card/CardsList"
-            options={{
-              drawerLabel: "Cards Listing",
-              title: "Cards Listing",
+    <GestureHandlerRootView style={{ flex: 1 }}>
+
+      {
+        onlyShowVerificationScreen ? (
+          <Stack
+            initialRouteName="index"
+            screenOptions={{
+              headerShown: true,
             }}
-          />
-          <Drawer.Screen
-            name="index"
-            options={{
-              drawerLabel: "Verification",
-              title: "Verification",
-            }}
-          />
-          <Drawer.Screen
-            name="card/AddCard"
-            options={{
-              drawerLabel: "Add Card",
-              title: "Add Card",
-            }}
-          />
-        </Drawer>
-      </GestureHandlerRootView>
+          >
+            <Stack.Screen
+              name="index"
+              options={{
+                title: "Verification",
+              }}
+            />
+          </Stack>
+        ):(
+          <Drawer>
+            <Drawer.Screen
+              name="index"
+              options={{
+                drawerLabel: "Verification",
+                title: "Verification",
+              }}
+            />
+            
+                  <Drawer.Screen
+                    name="card/CardsList"
+                    options={{
+                      drawerLabel: "Cards Listing",
+                      title: "Cards Listing",
+                    }}
+                  />
+                  <Drawer.Screen
+                    name="card/AddCard"
+                    options={{
+                      drawerLabel: "Add Card",
+                      title: "Add Card",
+                    }}
+                  />
+              
+          </Drawer>
+        )
+      }
+    </GestureHandlerRootView>
   );
 }
